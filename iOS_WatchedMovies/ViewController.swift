67645096e87320a1e1c507b1a84b5f6f89b44b3f
roanacla <roanacla@me.com>
2021-05-27 17:41:39 -0700

    Remove Story Board

--- iOS_WatchedMovies/ViewController.swift
+++ iOS_WatchedMovies/ViewController.swift
@@ -9,19 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  //MARK: - Properties
   var image: UIImageView!
+  var tableView: UITableView!
   
+  
+  //MARK: - View Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    configureUITableView()
+    configureMainView()
+  }
+  
+  
+  //MARK: - Functions
+  private func configureMainView() {
+    self.view.backgroundColor = .systemBackground
+  }
+  
+  private func configureUITableView() {
+    self.tableView = UITableView(frame: view.bounds)
+    self.tableView.delegate = self
+    self.tableView.dataSource = self
+    self.view.addSubview(tableView)
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      tableView.topAnchor.constraint(equalTo: view.topAnchor),
+      tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+      tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+    ])
+  }
+  
+  private func configureUIImageView() -> UIImageView {
     let width = 300
     let height = 300
-    self.image = UIImageView(frame: CGRect(x: view.center.x - CGFloat(width / 2),
-                                           y: view.center.y - CGFloat(height / 2),
-                                           width: 300,
-                                           height: 300))
-    self.view.addSubview(image)
-    self.view.backgroundColor = .systemBackground
-    self.getMovieData(movieName: "Army of the dead")
+    return UIImageView(frame: CGRect(x: view.center.x - CGFloat(width / 2),
+                                     y: view.center.y - CGFloat(height / 2),
+                                     width: 300,
+                                     height: 300))
   }
   
   func getMovieData(movieName: String) {
@@ -48,7 +75,16 @@ class ViewController: UIViewController {
       }
     }
   }
+}
 
-
+//MARK: - UITableView Delegates
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
 }
 
